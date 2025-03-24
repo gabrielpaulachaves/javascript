ba42ec4b443f6d468739fcc9a0012cdeffd785e7
Gabriel <bielpchaves@gmail.com>
2025-03-24 11:59:24 -0300

    js

--- exercicios/ex003.js
+++ exercicios/ex003.js
@@ -1,2 +1,14 @@
 "use strict"
 
+
+function teste(){
+    let nome="sigmazil"
+if(true){
+    console.log("Dentro do if e do teste: " + nome)
+    }
+    console.log("dentro de teste: " + nome)
+}
+
+teste ()
+
+console.log("fora de teste: " + nome)
