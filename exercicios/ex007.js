936c4866556d70dcbdd6c077d04d7114b400bef8
Gabriel <bielpchaves@gmail.com>
2025-03-26 10:16:29 -0300

    js

--- exercicios/ex007.js
+++ exercicios/ex007.js
@@ -6,4 +6,11 @@ n2=6
 n3=2
 n4=12
 
-console.log((n1>n2))
+
+if(n1<n2){
+    console.log(n1 + " é superior a " + n2)
+}else{
+    console.log(n1 + " é inferior a " + n2)
+}
+console.log((n1>n2)||(n1>n4))
+
