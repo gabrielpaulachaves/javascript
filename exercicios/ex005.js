abc537a84151fbcaa8021bdbd3352a6c7e50f73c
Gabriel <bielpchaves@gmail.com>
2025-03-26 09:38:39 -0300

    js

---
+++ exercicios/ex005.js
@@ -0,0 +1,9 @@
+"use restrict"
+
+let num1=12
+let num2=7
+let num3=7
+
+
+console.log(num2 >= num2)
+console.log(num1 != num3)
---
+++ exercicios/ex007.js
@@ -0,0 +1,9 @@
+
+
+let n1,n2,n3,n4
+n1=10
+n2=6
+n3=2
+n4=12
+
+console.log((n1>n2))
