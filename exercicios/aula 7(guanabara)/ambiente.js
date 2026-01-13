80c7f95cdc18a232917d82938262f4cba36f4736
Gabriel <bielpchaves@gmail.com>
2026-01-13 19:58:26 -0300

    while

---
+++ exercicios/aula 7(guanabara)/ambiente.js
@@ -0,0 +1,23 @@
+
+let c = 10;
+
+
+while(c > 0){
+    console.log("Contagem regressiva para lançamento em: ", c) 
+    c-- 
+}
+if(c == 0){
+    console.log(c)
+    console.log("Bomba lançada!")
+}
+
+let pizza = 8
+
+do{
+    console.log(`Há ${pizza} fatias de pizza, comi uma fatia`)
+     pizza-- 
+    console.log(`Agora existem só ${pizza} fatias de pizza`)
+}while(pizza > 0)
+if(pizza == 0){
+    console.log("As fatias acabaram!")
+}    
