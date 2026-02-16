25d3798cb23322b40a46c29836f1bf6f9701ba43
Gabriel <bielpchaves@gmail.com>
2026-02-16 20:19:11 -0300

    closure final

--- exercicios/aula 12/closures002.js
+++ exercicios/aula 12/closures002.js
@@ -7,9 +7,9 @@ function vida(n){
             hp--
         console.log(`${jogador} tomou 1 HIT, agora ele possui ${hp} pontos de vida`)
         
-        if(hp==0){
+         if(hp==0){
             console.log(`game over`)
-        }
+         }
     }else{
             console.log(`${jogador} já foi derrotado`)
         }
@@ -23,12 +23,16 @@ function vida(n){
 const jogador1 = vida("jogador 1")
 const jogador2 = vida("jogador 2")
 
+/*Executa → atualiza hp
+Mas você não vê o valor retornado. */
 jogador1()
-jogador1()
-jogador1()
-jogador1()
-jogador2()
-jogador2()
+
+/*Executa → atualiza hp
+E mostra o valor retornado.*/
+console.log(jogador1())
+
+
+/*Resumo final da closure, quando criamos const jogador1 = vida("jogador 1"), a função vida() executa e cria um ambiente. Normalmente, quando a função termina, esse ambiente é apagado, porém, a função interna ainda está ali utilizando deste ambiente(utilizando as variaveis desse ambiente), então ele não pode ser apagado. O ambiente não é descartado  */
 
 
 
