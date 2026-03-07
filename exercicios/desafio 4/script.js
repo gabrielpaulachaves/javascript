f4f8f9dfd8fc1b14b0227a7836dbd7d8565a56c6
Gabriel <bielpchaves@gmail.com>
2026-03-07 15:00:39 -0300

    banco de horas att

--- exercicios/desafio 4/script.js
+++ exercicios/desafio 4/script.js
@@ -1,58 +1,46 @@
-let inicio = document.getElementById('inicio')
-let fim = document.getElementById('fim')
 let calcular = document.getElementById('calcular')
 let resultado = document.getElementById('resultado')
 
-calcular.addEventListener("click", () =>{
-const parte1 = inicio.value.split(':')
-let hora1 = Number(parte1[0])
-let min1 = Number(parte1[1])
-
-const parte2 = fim.value.split(':')
-let hora2 = Number(parte2[0])
-let min2 = Number(parte2[1])
-    
-let InicioMinutos = hora1 * 60 + min1
-let FimMinutos = hora2 * 60 + min2
-if(FimMinutos < InicioMinutos){
-    FimMinutos += 1440
+function BancoDeHoras(inicio, fim){
+    this.inicio = inicio,
+    this.fim = fim
 }
 
-let horatrabalhada = FimMinutos - InicioMinutos
-let cargahorariamax = 480
-if(horatrabalhada > 480){
-    console.log("positivado")
-}else if( horatrabalhada == 480){
-    console.log('Trabalho normal')
+BancoDeHoras.prototype.calculo = function(){
+let iniciovalor = this.inicio.split(':')
+let iniciohora = Number(iniciovalor[0])
+let iniciomin = Number(iniciovalor[1])
+
+let fimvalor = this.fim.split(':')
+let fimhora = Number(fimvalor[0])
+let fimmin = Number(fimvalor[1])
+
+let iniciotempo = iniciohora * 60 + iniciomin
+let fimtempo = fimhora * 60 + fimmin
+if(fimtempo < iniciotempo){
+    fimtempo += 1440
+}
+let horatrabalhada = fimtempo - iniciotempo
+let horamaxima = 480
+
+let saldo = horatrabalhada - horamaxima
+let valor = Math.abs(saldo)
+
+let divisasaldo = Math.floor(valor / 60)
+let restosaldo = (valor % 60).toString().padStart(2, '0')
+
+if(saldo > 0){
+   resultado.innerHTML = `O funcionário trabalhou mais do que deveria,e agora possui +${divisasaldo}h${restosaldo}m em seu banco de horas`
+}else if( saldo == 0){
+    resultado.innerHTML = `trabalho normal`
 }else{
-    console.log('negativado')
+    resultado.innerHTML = `O funcionário trabalhou menos do que deveria,e agora possui -${divisasaldo}h${restosaldo}m em seu banco de horas`
+}
 }
 
-let saldo = horatrabalhada - cargahorariamax
 
-let divisasaldo = 
-let restosaldo = 
-
+calcular.addEventListener('click', function(){  
+let horario = new BancoDeHoras(document.getElementById('inicio').value, document.getElementById('fim').value)     
+horario.calculo()
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
