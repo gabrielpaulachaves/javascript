015d106ead68335c9495fe69e1597720225f5d15
Gabriel <bielpchaves@gmail.com>
2026-03-12 11:04:44 -0300

    map

--- exercicios/aula 16/script.js
+++ exercicios/aula 16/script.js
@@ -1,12 +1,17 @@
 /*MAP, FILTER e REDUCE */
 
+/*MAP*/
+/*sintaxe do map:   'array'.map(('array nova')=>{
+    return 'array' 'operação que queremos'*/
+/*Podemos colocar ele em uma variavel, como no exemplo a seguir */
 
+let preco = [6, 6, 1, 16, 43, 82]
+let precoComDesconto = preco.map((precos)=>{
+    return precos * 0.5
+})
 
-
-
-
-
-
+console.log(preco)
+console.log(precoComDesconto)
 
 
 
