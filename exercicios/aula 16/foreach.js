dbae619e8ae4516ff1fcb6c876295fb91b9db945
Gabriel <bielpchaves@gmail.com>
2026-03-16 20:06:09 -0300

    foreach

---
+++ exercicios/aula 16/foreach.js
@@ -0,0 +1,13 @@
+/*
+    item = dados/informação no array
+    index = posição dele (o indice)
+    array = retorna o array inteiro
+*/
+
+const usuarios =[
+    {nome:'', },
+    {nome:'', },
+    {nome:'', },
+    {nome:'', },
+    {nome:'', }
+]
